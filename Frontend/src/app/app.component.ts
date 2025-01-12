8e4606ba70f9e09f69f9759c7bb89caf516a7919
Abdelali TAHA <abdeltahaali@gmail.com>
2025-01-12 18:03:08 +0100

    Ebankify Fron 2.0

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HttpClientModule],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'FRONTEND';
+}
